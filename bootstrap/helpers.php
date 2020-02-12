4a4363c6f4e7c6babf0f2995bdfe9f8d40628364
guilin <1141768780@qq.com>
2020-02-12 11:46:37 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,9 @@
 function test_helper() {
     return 'OK';
 }
+
+// 将请求的路由名称转换为CSS类名称
+function route_class()
+{
+    return str_replace('.','-',\Illuminate\Support\Facades\Route::currentRouteName());
+}
