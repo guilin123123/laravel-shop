325b200e1216cdbb6ad2dc734ac072a3565ac294
guilin <1141768780@qq.com>
2020-02-13 15:53:40 +0000

    商品模型

---
+++ app/Models/ProductSku.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSku extends Model
+{
+    protected $fillable = [
+        'title', 'description', 'price', 'stock'
+    ];
+
+    public function product()
+    {
+        $this->belongs(Product::class);
+    }
+}
