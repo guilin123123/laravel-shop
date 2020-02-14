fbb169759bbea8de86722417668280894979ee7c
guilin <1141768780@qq.com>
2020-02-14 15:08:18 +0000

    用户界面 - 商品筛选,排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,13 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        $bulider = Product::query()->where('on_sale', true);
 
-        return view('products.index', ['products' => $products]);
+        if ($search = $request->input('search','')) {
+            $like = '%'.$search.'%';
+
+            // 模糊搜索商品标题 商品详情 SKU标题 SKU描述
+            $bulider->where(function ($query) use($like){
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use($like){
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        if ($order = $request->input('order','')) {
+            // 是否以_asc或者_desc结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这3个字符串之一,说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根据传入的排序值来构造排序参数
+                    $bulider->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $bulider->paginate(16);
+
+        return view('products.index', [
+            'products' => $products,
+            'filters' => [
+                'search' => $search,
+                'order' => $order,
+            ],
+        ]);
     }
 }
