baa0a935c5a372e0856d4881f4e177a3d1ebb9c7
guilin <1141768780@qq.com>
2020-02-15 15:26:10 +0000

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::group(['middleware' => [
    Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
-
+   Route::post('cart', 'CartController@add')->name('cart.add');
 });
 Route::redirect('/','/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
