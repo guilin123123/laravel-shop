e59a3599a3a7c28134716564dc6e5544ff9b3012
guilin <1141768780@qq.com>
2020-02-17 15:10:09 +0000

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
@@ -72,6 +73,10 @@ class OrdersController extends Controller
 
             return $order;
         });
+
+        // 执行队列任务
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
+
         return $order;
     }
 }
