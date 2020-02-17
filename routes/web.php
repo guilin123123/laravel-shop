19fbc1da5e889aacbc85e19488018707fd60c14e
guilin <1141768780@qq.com>
2020-02-17 15:19:12 +0000

    用户界面 - 订单列表项

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::group(['middleware' => [
    Route::get('cart', 'CartController@index')->name('cart.index');
    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
    Route::post('orders', 'OrdersController@store')->name('orders.store');
+   Route::get('orders', 'OrdersController@index')->name('orders.index');
 });
 Route::redirect('/','/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
